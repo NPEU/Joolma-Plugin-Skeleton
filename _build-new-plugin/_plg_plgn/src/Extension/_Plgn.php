03e573c97b4f67dad21f3ef53603c0602de1ac94
Andy Kirk <andy@andykirk.net>
2024-03-25 15:55:51 +0000

    Updated skeleton

--- _build-new-plugin/_plg_plgn/src/Extension/_Plgn.php
+++ _build-new-plugin/_plg_plgn/src/Extension/_Plgn.php
@@ -33,6 +33,24 @@ class _Plgn extends CMSPlugin implements SubscriberInterface
     protected static $enabled = false;
 
     /**
+     * Constructor
+     *
+     */
+    public function __construct($subject, array $config = [], bool $enabled = true)
+    {
+        // The above enabled parameter was taken from the Guided Tour plugin but it always seems
+        // to be false so I'm not sure where this param is passed from. Overriding it for now.
+        $enabled = true;
+
+
+        #$this->loadLanguage();
+        $this->autoloadLanguage = $enabled;
+        self::$enabled          = $enabled;
+
+        parent::__construct($subject, $config);
+    }
+
+    /**
      * function for getSubscribedEvents : new Joomla 4 feature
      *
      * @return array
@@ -56,4 +74,36 @@ class _Plgn extends CMSPlugin implements SubscriberInterface
         Factory::getApplication()->enqueueMessage('Testing', 'notice');
         return true;
     }
+
+    /**
+     * Prepare form and add my field.
+     *
+     * @param   Form  $form  The form to be altered.
+     * @param   mixed  $data  The associated data for the form.
+     *
+     * @return  boolean
+     *
+     * @since   <your version>
+     */
+    /*public function onContentPrepareForm(Event $event): void
+    {
+        $args    = $event->getArguments();
+        $form    = $args[0];
+        $data    = $args[1];
+
+        if (!($form instanceof \Joomla\CMS\Form\Form)) {
+            throw new GenericDataException(Text::_('JERROR_NOT_A_FORM'), 500);
+            return false;
+        }
+
+        $app    = Factory::getApplication();
+        $option = $app->input->get('option');
+
+        if ($app->isClient('administrator') && $option == 'com_CHECK') {
+            FormHelper::addFormPath(dirname(dirname(__DIR__)) . '/forms');
+            $form->loadFile('menu_item', false);
+        }
+
+        // ...
+    }*/
 }
